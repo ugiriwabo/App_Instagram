b9fa8ea3ae5612142dba2d4e25309d56f8dcd548
ugiriwabo <ugiraconsole@gmail.com>
2019-03-26 17:33:14 +0200

    now i can login and logout

--- photoes/urls.py
+++ photoes/urls.py
@@ -1,11 +1,7 @@
 from django.conf.urls import url, include
-from django.contrib.auth import views 
 from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
-    url('^today/$',views.news_of_day,name='newsToday'),
-    url(r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'^logout/$', views.logout, {"next_page": '/'}),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews')
+    url('^today/$',views.news_of_day,name='newsToday'),    
 ]
