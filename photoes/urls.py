bf0608591e29805fa07a3153280af55b4f39e2b8
ugiriwabo <ugiraconsole@gmail.com>
2019-03-27 09:04:42 +0200

    remove some file

--- photoes/urls.py
+++ photoes/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
-    url('^today/$',views.news_of_day,name='newsToday'),    
+    url('^$',views.welcome,name = 'welcome'),    
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- photoes/views.py
+++ photoes/views.py
@@ -6,36 +6,3 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='/accounts/login/')
 def welcome(request):
     return render(request, 'welcome.html')
-
-def news_of_day(request):
-    date = dt.date.today()
-    return render(request, 'all-instagram/today-news.html', {"date": date,})
-
-def past_days_news(request, past_date):
-
-    try:
-        # Converts data from the string Url
-        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
-        day = convert_dates(date)
-    except ValueError:
-        # Raise 404 error when ValueError is thrown
-        raise Http404()
-        assert False
-
-    if date == dt.date.today():
-        return redirect(news_of_day)
-
-    return render(request, 'all-news/past-news.html', {"date": date})
-
-def convert_dates(dates):
-
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
-
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
-
-
